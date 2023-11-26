6ba94d91b041c62b9936d23b7c3a1711b17b5333
Ahmed Fkri <ahmedfkri@hotmail.com>
2023-11-26 20:16:11 +0200

    fixing

--- app/src/main/java/com/example/todo/presentation/MainActivity.kt
+++ app/src/main/java/com/example/todo/presentation/MainActivity.kt
@@ -49,7 +49,6 @@ class MainActivity : AppCompatActivity() {
         navController.navigate(SplashFragmentDirections.actionSplashFragmentToToDoListFragment())
     }
 
-    println("newLine")
 
 
 }
